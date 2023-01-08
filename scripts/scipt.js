cd8e6cb80cf763add8bbb58d23e400605cd1349b
darizae <danielarizae@gmail.com>
2023-01-08 20:09:07 +0100

    Implement getComputerOption function
    Declare a string array where all the playing options are stored.
    Declare a function that returns a random index number of the options
    array. Declare a function that returns a randomly generated playing
    option.

--- scripts/scipt.js
+++ scripts/scipt.js
@@ -1,5 +1,18 @@
 const button = document.getElementById('playButton');
 
 button.addEventListener("click", function() {
-    alert("Button clicked");
+    alert(getComputerChoice());
 });
+
+//Choice options
+const options = ["rock", "paper", "scissors"];
+
+//Returns a random option from the options array
+function getComputerChoice() {
+    return options[getRandomNumber()];
+}
+
+//Generates random number between 0 and 2
+function getRandomNumber() {
+    return Math.floor(Math.random() * 3);
+}
